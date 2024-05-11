ec7b82a980de07e4d40a62d36b16995fee39be82
blejdeaxoxo <163322591+blejdeaxoxo@users.noreply.github.com>
2024-05-11 22:53:35 +0300

    Update verify_dangerous.sh

--- verify_dangerous.sh
+++ verify_dangerous.sh
@@ -1,15 +1,22 @@
 file=$1
 
-wc_output=$(wc -l -w -c "$file")
+chmod +r "$file"
+
+content=$(cat "$file")
+wc_output=$(echo "$content" | wc -l -w -c)
 
 line_count=$(echo "$wc_output" | awk '{print $1}')
 word_count=$(echo "$wc_output" | awk '{print $2}')
 char_count=$(echo "$wc_output" | awk '{print $3}')
 
-if [ "$line_count" -lt 3 ] && [ "$word_count" -gt 1000 ] && [ "$char_count" -gt 2000 ]; then 
+if [ "$line_count" -lt 3 ] && [ "$word_count" -gt 1000 ] && [ "$char_count" -gt 2000 ]; then
     if grep -qP '[^\x00-\x7F]' "$file" || grep -qiE "corrupted|dangerous|risk|attack|malware|malicious" "$file"; then
         echo "$file"
     else echo "SAFE"
     fi
 else echo "SAFE"
 fi
+
+chmod 000 "$file"
+
+exit 0
